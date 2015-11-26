e4012e5896ef39c01a30831efe84dc945bf57f9e
Tomás de Camino Beck <tomas.decamino@gmail.com>
2015-11-26 08:07:26 -0600

    Create Read_Serial.ino

---
+++ Read_Serial.ino
@@ -0,0 +1,22 @@
+// @author Tomas de Camino Beck
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char inChar;
+  if (Serial.available()) {
+    inChar = (char)Serial.read();
+    if (inChar == '0') {
+      digitalWrite(13,LOW);
+    }
+    else{
+      digitalWrite(13,HIGH);
+    }
+
+  }
+
+}
